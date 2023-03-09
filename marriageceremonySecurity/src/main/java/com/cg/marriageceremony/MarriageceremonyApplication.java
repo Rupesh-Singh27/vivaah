d3b23c8a32e6a3448d27b6f70420b794cdf6b410
Rupesh-Singh27 <76389171+Rupesh-Singh27@users.noreply.github.com>
2023-03-09 15:30:16 +0530

    Update MarriageceremonyApplication.java

--- marriageceremonySecurity/src/main/java/com/cg/marriageceremony/MarriageceremonyApplication.java
+++ marriageceremonySecurity/src/main/java/com/cg/marriageceremony/MarriageceremonyApplication.java
@@ -17,7 +17,6 @@ public class MarriageceremonyApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-			//	registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
 				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
